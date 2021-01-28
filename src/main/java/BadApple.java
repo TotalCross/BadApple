f17f755047af32ed326108bbd04b19d4f6b0dde3
Fabio <flsobral@gmail.com>
2021-01-28 12:39:02 -0300

    merges the out files into a single one, that can be read faster

--- src/main/java/BadApple.java
+++ src/main/java/BadApple.java
@@ -6,8 +6,11 @@ import totalcross.ui.event.PressListener;
 import totalcross.ui.event.UpdateListener;
 import totalcross.ui.gfx.Color;
 import totalcross.ui.gfx.Graphics;
-
+import totalcross.io.BufferedStream;
 import totalcross.io.ByteArrayStream;
+import totalcross.io.DataStream;
+import totalcross.io.File;
+import totalcross.io.IOException;
 import totalcross.io.LineReader;
 import totalcross.sys.Convert;
 import totalcross.sys.Settings;
@@ -15,8 +18,23 @@ import totalcross.sys.Vm;
 
 public class BadApple extends MainWindow {
 
+    DataStream input = null;
+
     public BadApple() {
         setUIStyle(Settings.MATERIAL_UI);
+        // try {
+        // convert(null);
+        // } catch (Exception e) {
+        // // TODO Auto-generated catch block
+        // e.printStackTrace();
+        // }
+        try {
+            // File f = new File("data/out.txt", File.READ_ONLY);
+            input = new DataStream(new ByteArrayStream(Vm.getFile("data/out.txt")));
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
     long timestamp = 0;
@@ -104,37 +122,84 @@ public class BadApple extends MainWindow {
 
         // FileReader fr = null;
         // BufferedReader br = null;
-        LineReader reader = null;
+        BufferedStream reader = null;
         ByteArrayStream bas = null;
         try {
-
-            bas = new ByteArrayStream(Vm.getFile(fileName));
-            reader = new LineReader(bas);
-
-            String temp;
-
-            int j = 0;
-
-            while ((temp = reader.readLine()) != null) {
-                String[] data = Convert.tokenizeString(temp, " ");
-                int w = 0;
-                while (w < data.length) {
-                    int start = Convert.toInt(data[w++]);
-                    int end = Convert.toInt(data[w++]);
-                    g.drawLine(start, j, end, j);
+            for (int i = 0 ; i < 480 ; i++) {
+                for (int j = input.readShort() ; j > 0 ; j--) {
+                    g.drawLine(input.readShort(), i, input.readShort(), i);
                 }
-
-                j++;
             }
+
+            // bas = new ByteArrayStream(Vm.getFile(fileName));
+            // reader = new BufferedStream(bas, BufferedStream.READ, 4096);
+
+            // String temp;
+
+            // int j = 0;
+
+            // while ((temp = reader.readLine()) != null) {
+            //     if (temp.length() > 2) {
+            //         String[] data = Convert.tokenizeString(temp, " ");
+            //         int w = 0;
+            //         while (w < data.length) {
+            //             int start = Convert.toInt(data[w++]);
+            //             int end = Convert.toInt(data[w++]);
+            //             g.drawLine(start, j, end, j);
+            //         }
+            //     }
+
+            //     j++;
+            // }
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            try {
-                bas.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            // try {
+            //     bas.close();
+            // } catch (Exception e) {
+            //     e.printStackTrace();
+            // }
         }
     }
 
+
+    public static void convert(String[] args) throws Exception {
+        ByteArrayStream out = new ByteArrayStream(4096);
+        DataStream ds = new DataStream(out);
+        
+        byte[] frame = null;
+        for(int i = 1 ; (frame = Vm.getFile("data/out (" + i + ").txt")) != null ; i++) {
+            ByteArrayStream bas = null;
+
+            bas = new ByteArrayStream(frame);
+            BufferedStream reader = new BufferedStream(bas, BufferedStream.READ, 4096);
+
+            String temp;
+
+            if (i == 129) {
+                int a = 10;
+                a++;
+            }
+
+            for (int j = 0 ; (temp = reader.readLine()) != null ; j++) {
+                if (temp.length()  < 2) {
+                    ds.writeShort(0);
+                    continue;
+                }
+                String[] data = Convert.tokenizeString(temp, " ");
+                int w = 0;
+                ds.writeShort(data.length / 2);
+                while (w < data.length) {
+                    int start = Convert.toInt(data[w++]);
+                    int end = Convert.toInt(data[w++]);
+                    ds.writeShort(start);
+                    ds.writeShort(end);
+                    // g.drawLine(start, j, end, j);
+                }
+            }
+        }
+        File f = new File("out.txt", File.CREATE_EMPTY);
+        f.writeBytes(out.getBuffer(), 0, out.getPos());
+        f.close();
+    }
 }
